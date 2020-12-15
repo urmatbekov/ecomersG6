f1dbfa026800090142b3a8015f83aa0ab66d41be
nurkadyr <nurkadyrurmatbekov@gmail.com>
2020-12-15 12:18:31 +0600

    first commit

--- ecomersG6/settings.py
+++ ecomersG6/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 from oscar.defaults import *
 from pathlib import Path
-from settings_prod import *
+from ecomersG6.settings_prod import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
