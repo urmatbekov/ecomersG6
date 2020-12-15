38bdd934e113250a4cb38a898a588a38a6b39c88
nurkadyr <nurkadyrurmatbekov@gmail.com>
2020-12-15 12:27:01 +0600

    first commit

--- ecomersG6/settings.py
+++ ecomersG6/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import os
+
 from oscar.defaults import *
 from pathlib import Path
 from ecomersG6.settings_prod import *
@@ -202,4 +204,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_root/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
